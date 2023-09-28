a3dc15042cb24dc88d5c04dd0b2d49ffaacc2477
“Huiting <huitingloke@gmail.com>
2023-09-28 17:59:19 +0800

    INITIAL COMMIT
    SALDKFJALSKJDFAKLSDFJKLASDF IT WORKS LDFKJASD;LFKASD;KLFASD;KLFJ

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,62 @@
+<script>
+  import { Sound } from "svelte-sound";
+  import boom from "../assets/boom.wav";
+  import chak from "../assets/chak.wav";
+  import softcym from "../assets/softcymbal.mp3";
+  import loudcym from "../assets/loudcymbal.mp3";
+
+  const boom_sound = new Sound(boom);
+  const chak_sound = new Sound(chak);
+  const softcym_sound = new Sound(softcym);
+  const loudcym_sound = new Sound(loudcym);
+
+  function playChak() {
+    console.log("chak")
+    chak_sound.play();
+  } // playSound can be called anywhere in the code
+
+  const playBoom = () => {
+    console.log("boom");
+    boom_sound.play();
+  }
+  const playSoftCym = () => {
+    console.log("soft");
+    softcym_sound.play();
+  }
+  const playLoudCym = () => {
+    console.log("loud");
+    loudcym_sound.play();
+  }
+
+</script>
+
+<main>
+  <div>
+    <button on:click={playLoudCym}>
+      loud
+    </button>
+    <button on:click={playBoom}>
+      boom
+    </button>
+    <button on:click={playBoom}>
+      boom
+    </button>
+    <button on:click={playLoudCym}>
+      loud
+    </button>
+  </div>
+  <div>
+    <button on:click={playSoftCym}>
+      soft
+    </button>
+    <button on:click={playChak}>
+      chak
+    </button>
+    <button on:click={playChak}>
+      chak
+    </button>
+    <button on:click={playSoftCym}>
+      soft
+    </button>
+  </div>
+</main>
